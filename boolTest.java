719776588a784b004dafe6a5ec964a0507a5f242
pushpitshukla28 <pushpitshukla28@gmail.com>
2024-09-25 19:17:27 +0530

    Boolean Try

---
+++ boolTest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class boolTest {
+    public static void main(String[] args) {
+        
+    
+    boolean b;
+    b= true;
+    if(b){
+        System.out.println("This will execute");
+    }
+    b = false;
+    if(b){
+        System.out.println("This will not execute");
+    }
+    
+    System.out.println("10>9 is " +(10>9));
+
+
+}
+}
